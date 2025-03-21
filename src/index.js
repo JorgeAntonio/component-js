856a04c7988795bf1aaed99b1285698008840fff
JorgeAntonio <jorge.antonio.delaguila@gmail.com>
2025-03-20 23:04:28 -0500

    Update package.json, add TextFormField component, and configure Vite for library build

---
+++ src/index.js
@@ -0,0 +1,1 @@
+export { default as TextFormField } from "./components/text-form-field.jsx";
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,37 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.js"),
+      name: "ComponentJS",
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM"
+        }
+      }
+    }
+  }
+});
+
